2612d1c3a23e419abc7826e32db6719c96399d1c
Xinrea <xinreas@outlook.com>
2025-04-28 20:52:53 +0800

    feat: new dynamic emojis

--- src/components/EmojiPicker.svelte
+++ src/components/EmojiPicker.svelte
@@ -1,18 +1,47 @@
 <script>
   let emojis = [
-    { id: 0, value: "👍"},
-    { id: 1, value: "👎"},
-    { id: 2, value: "🤣"},
-    { id: 3, value: "😭"},
-    { id: 4, value: "😓"},
-    { id: 5, value: "😬"},
-    { id: 6, value: "🥳"},
-    { id: 7, value: "😨"},
-    { id: 8, value: "😠"},
-    { id: 9, value: "💩"},
-    { id: 10, value: "💖"},
-    { id: 11, value: "🐵"},
+    { id: 0, value: "👍", asset: "emojis/76.png" },
+    { id: 1, value: "👎", asset: "emojis/77.png" },
+    { id: 2, value: "🤣" },
+    { id: 3, value: "😭" },
+    { id: 4, value: "😓" },
+    { id: 5, value: "😬" },
+    { id: 6, value: "🥳" },
+    { id: 7, value: "😨" },
+    { id: 8, value: "😠" },
+    { id: 9, value: "💩", asset: "emojis/59.png" },
+    { id: 10, value: "💖", asset: "emojis/66.png" },
+    { id: 11, value: "🐵" },
+    { id: 12, value: "❓" },
+    { id: 13, value: "🫂", asset: "emojis/49.png" },
+    { id: 14, value: "🔘", asset: "emojis/424.png" },
+    { id: 15, value: "👅", asset: "emojis/339.png" },
+    { id: 16, value: "🥺", asset: "emojis/111.png" },
+    { id: 17, value: "👻", asset: "emojis/187.png" },
+    { id: 18, value: "😅" },
+    { id: 19, value: "🌹", asset: "emojis/63.png" },
   ];
+  let emojisMap = {
+    "👍": "emojis/76.png",
+    "👎": "emojis/77.png",
+    "🤣": null,
+    "😭": null,
+    "😓": null,
+    "😬": null,
+    "🥳": null,
+    "😨": null,
+    "😠": null,
+    "💩": "emojis/59.png",
+    "💖": "emojis/66.png",
+    "🐵": null,
+    "❓": null,
+    "🫂": "emojis/49.png",
+    "🔘": "emojis/424.png",
+    "👅": "emojis/339.png",
+    "🥺": "emojis/111.png",
+    "👻": "emojis/187.png",
+    "😅": null,
+  };
   export let questionID;
   export let data = [];
   let panel = {
@@ -22,14 +51,14 @@
   function togglePanel() {
     panel.open = !panel.open;
     console.log("toggle emoji panel");
-  };
+  }
   function doPostEmoji(value) {
     panel.open = false;
     if (posted) {
       return;
     }
     let formData = new FormData();
-    formData.append("emoji", value)
+    formData.append("emoji", value);
     fetch(`/api/question/${questionID}/emoji`, {
       credentials: "include",
       method: "POST",
@@ -49,44 +78,51 @@
           if (!added) {
             data.push({ value: value, count: 1 });
           }
-          data = data.sort((a, b)=>{
+          data = data.sort((a, b) => {
             return b.count - a.count;
           });
         } else {
-          console.error(res)
+          console.error(res);
         }
       });
   }
   function postEmoji(value) {
-    return ()=>{
+    return () => {
       doPostEmoji(value);
-    }
+    };
   }
-  data = data.sort((a, b)=>{
+  data = data.sort((a, b) => {
     return b.count - a.count;
   });
 </script>
 
 <div class="picker inline-block select-none">
   <div class="panel flex flex-row" class:invisible={!panel.open}>
-    {#each emojis as {id, value}}
+    {#each emojis as { id, value, asset }}
       <div class="emoji-button" on:click={postEmoji(value)}>
-        {value}
+        {#if asset}
+          <img src={asset} alt={value} height="16px" width="16px" />
+        {:else}
+          {value}
+        {/if}
       </div>
     {/each}
   </div>
   <div class="flex flex-row">
-    {#each data as {value, count}}
+    {#each data as { value, count }}
       <div class="emoji-button" on:click={postEmoji(value)}>
-        {value}
+        {#if emojisMap[value]}
+          <img src={emojisMap[value]} alt={value} height="16px" width="16px" />
+        {:else}
+          {value}
+        {/if}
         <span class="counter">
           {count}
         </span>
       </div>
     {/each}
     <div class="emoji-button">
-      <div class="add-emoji bg-current" on:click={togglePanel}>
-      </div>
+      <div class="add-emoji bg-current" on:click={togglePanel}></div>
     </div>
   </div>
 </div>
@@ -127,5 +163,4 @@
     background-color: white;
     padding: 0.6rem;
   }
-
 </style>
